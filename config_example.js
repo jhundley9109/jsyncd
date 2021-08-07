a43d19f4345f4ed2a1132abafcd36e427e87dd8f
Jacob Hundley <jhundley@eretailing.com>
2021-08-07 12:56:58 -0400

    Add more configurations, made it much more efficient, and support for all kinds of ssh, rsync, and chokidar options.

--- config_example.js
+++ config_example.js
@@ -2,21 +2,28 @@
 /*jshint esversion: 6 */
 /*jslint node: true */
 
-// Rename copy this file into ~/.config/jsyncd/config.js
-
 let config = {
-  rsyncLocation: '/usr/local/bin/rsync',
-  appConfig: [{
-    server: 'localhost',
-    port: '22', // optional
-    targetUsername: 'user_name',
-    identityFile: '/home/user_name/.ssh/identityfile',
-    directories: [{
-      targetDir: '/remote/path/',
-      localDir: '/home/user_name/git/your_project/',
-      excludePattern: ['*.tmp', '*/node/*', '.auth', 'node_modules'] // files you do not wish to be synced
-    }]
-  }],
+	appConfig: [{
+		hostConfig: {
+			hostname: 'localhost',
+			targetUsername: 'user_name',
+			sshOptions: {
+				'-p': '2222',
+				'-i': '/home/user_name/.ssh/identity_file',
+			}
+		},
+		directories: [{
+			targetDir: '/var/some_project/',
+			localDir: '/home/user_name/git/some_project/',
+			excludePattern: ['*.tmp', '*/node/*', '.auth', 'node_modules', '.git']
+		}],
+		chokidarOptions: {
+			ignoreInitial: false,
+			ignored: [/node_modules/, /\.git/]
+		},
+		// recommend a and i as defaults
+		rsyncFlags: ['a', 'O', 'i']
+	}],
 }
 
 module.exports = config;
--- start_sync.js
+++ start_sync.js
@@ -10,12 +10,43 @@ let config = require(process.env.HOME + '/.config/jsyncd/config');
 const Rsync = require('rsync');
 const chokidar = require('chokidar');
 const moment = require('moment');
+const fs = require('fs');
+
+let logFileHandle = null;
+try {
+	// logFileHandle = fs.openSync(config.logFile, 'a')
+	logFileHandle = fs.createWriteStream(config.logFile, {flags: 'a'});
+} catch (err) {
+	console.log('Cannot open log for writing with message: ' + err)
+	process.exit();
+}
 
 let watcherObj;
 
 config.appConfig.forEach((syncConfig) => {
-  let applicationFolders = syncConfig.directories;
+	let applicationFolders = syncConfig.directories;
+	let chokidarOptions = syncConfig.chokidarOptions || {};
 
+	let remoteHostUri = '';
+	let remoteConfig = syncConfig.hostConfig || {};
+	let sshConnectionString = '';
+
+	if (remoteConfig.targetUsername)
+	{
+		remoteHostUri += remoteConfig.targetUsername && remoteConfig.targetUsername + '@'
+		remoteHostUri += remoteConfig.hostname + ':';
+
+		let sshOptions = [];
+
+		for (const [key, value] of Object.entries(remoteConfig.sshOptions || {})) {
+			sshOptions.push(key, value)
+		}
+
+		if (sshOptions.length)
+		{
+			sshConnectionString += 'ssh ' + sshOptions.join(' ');
+		}
+	}
 
 	applicationFolders.forEach((directoryConfig) => {
 		let completeLocalSourceDir = directoryConfig.localDir;
@@ -24,54 +55,91 @@ config.appConfig.forEach((syncConfig) => {
 
 		let activeDirectorySyncs = {};
 
-		let rsync = new Rsync();
-		rsync.flags('r', 't', 'O', 'i');
+		watcherObj = chokidar.watch(completeLocalSourceDir, chokidarOptions).on('all', (event, localFileDir) => {
 
-		if (excludePattern.length)
-				rsync.exclude(excludePattern);
-
-		rsync.source(completeLocalSourceDir)
-		rsync.destination(syncConfig.targetUsername + '@' + syncConfig.server + ':' + completeTargetPath)
-
-		rsync.set('e', 'ssh -p ' + syncConfig.port + ' -i ' + syncConfig.identityFile)
-
-		watcherObj = chokidar.watch(completeLocalSourceDir).on('all', (event, localFileDir) => {
-			// only listen for these events for now.
+			// // only listen for these events for now.
+			// sendToLog('event: ' + event + ' local dir ' + localFileDir)
 			if (event != 'addDir' && event != 'change' && event != 'add')
 			{
 				return;
 			}
 
-			if (activeDirectorySyncs[completeLocalSourceDir]) {
-				// console.log("a sync is already queued for", completeLocalSourceDir);
+			if (activeDirectorySyncs[localFileDir]) {
+				sendToLog("a sync is already queued for", localFileDir);
 				return;
 			}
 
-			console.log(moment().format('ddd MMMM D YYYY H:mm:ss') + ' Calling rsync for ' + completeLocalSourceDir);
-			activeDirectorySyncs[completeLocalSourceDir] = true;
+			// foreach
+			let alreadyRunningOnParent = false;
+
+			let syncArray = Object.keys(activeDirectorySyncs);
+			// syncArray.every(activeSync => {
+			for (let activeSync of syncArray)
+			{
+				if (!alreadyRunningOnParent && activeDirectorySyncs[activeSync] && localFileDir.includes(activeSync))
+				{
+					alreadyRunningOnParent = true;
+					return;
+				}
+			}
+
+			if (alreadyRunningOnParent)
+			{
+				// sendToLog("***************************a sync is already queued for a parent directory")
+				return;
+			}
+
+			let rsync = new Rsync();
+			rsync.flags(syncConfig.rsyncFlags || []);
+
+			if (excludePattern.length)
+					rsync.exclude(excludePattern);
+
+			sshConnectionString && rsync.set('e', sshConnectionString);
+			rsync.destination(remoteHostUri + completeTargetPath)
+			rsync.source(localFileDir);
+
+			sendToLog(getTimestamp() + ' Calling rsync for ' + localFileDir);
+			activeDirectorySyncs[localFileDir] = true;
 
 			rsync.execute((error, code, cmd) => {
-				// we're done
-				activeDirectorySyncs[completeLocalSourceDir] = false;
-				console.log(moment().format('ddd MMMM D YYYY H:mm:ss') + ' Finished syncing ' + completeLocalSourceDir);
-				// console.log('rsync status', error, code, cmd)
+				activeDirectorySyncs[localFileDir] = false;
+
+				if (error)
+				{
+					sendToLog(getTimestamp() + ' Error syncing: ' + error);
+				}
+				else
+				{
+					sendToLog(getTimestamp() + ' Finished syncing ' + localFileDir);
+				}
 			}, (stdoutHandle) => {
-				// console.log("in stdout handle", stdoutHandle)
 				// process.stdout.prin
 				// rsync with the -i option has some weird junk at the beginning of the file name. Just trim that off.
+				// sendToLog(stdoutHandle.toString())
 				let formattedOutput = stdoutHandle.toString().replace(/<.*\.\. /g, '').trim();
-				console.log(formattedOutput)
-				// console.log(moment().format('ddd MMMM D YYYY H:mm:ss') + ' Finished syncing ' + cmd);
+
+				sendToLog('Rsync output: ' + formattedOutput)
+			}, (stderrHandle) => {
+				let formattedOutput = stderrHandle.toString().replace(/<.*\.\. /g, '').trim();
+				sendToLog('Rsync error: ' + formattedOutput)
 			});
 		});
 	});
 });
 
-process.on('SIGINT', () => {
-	console.log("caught it");
+function getTimestamp() {
+	return moment().format('ddd MMMM D YYYY H:mm:ss')
+}
 
+function sendToLog(contentToLog) {
+	logFileHandle.write(contentToLog + "\n")
+}
+
+process.on('SIGINT', () => {
 	watcherObj.close().then(() => {
-		console.log("Closed everything");
+		sendToLog("Closed everything");
+		fs.closeSync(logFileHandle);
 		process.exit();
 	});
 });
