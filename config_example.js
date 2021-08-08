fe3fdcd58f01740cd385ef3b9fb3187d231f2da5
Jacob Hundley <jhundley@eretailing.com>
2021-08-08 12:06:51 -0400

    And update example

--- config_example.js
+++ config_example.js
@@ -3,28 +3,30 @@
 /*jslint node: true */
 
 let config = {
-	logFile: '/var/log/jsyncd/jsyncd.log',
-	appConfig: [{
-		hostConfig: {
-			hostname: 'localhost',
-			targetUsername: 'user_name',
-			sshOptions: {
-				'-p': '2222',
-				'-i': '/home/user_name/.ssh/identity_file',
-			}
-		},
-		directories: [{
-			localDir: '/home/user_name/git/some_project/',
-			targetDir: '/var/some_project/',
-			excludePattern: ['*.tmp', '*/node/*', '.auth', 'node_modules', '.git']
-		}],
-		chokidarOptions: {
-			ignoreInitial: false,
-			ignored: [/node_modules/, /\.git/]
-		},
-		// recommend a and i as defaults
-		rsyncFlags: ['a', 'O', 'i']
-	}],
+  logFile: '/var/log/jsyncd/jsyncd.log',
+  daemonize: false,
+  appConfig: [{
+    hostConfig: {
+      hostname: 'localhost',
+      targetUsername: 'user_name',
+      sshOptions: {
+        '-p': '2222',
+        '-i': '/home/user_name/.ssh/identity_file',
+      }
+    },
+    directories: [{
+      source: '/var/some_project/',
+      destination: '/home/user_name/git/some_project/',
+      rsyncExcludePattern: ['*.tmp', '*/node/*', '.auth', 'node_modules', '.git']
+    }],
+    chokidarOptions: {
+      ignoreInitial: false,
+      ignored: [/node_modules/, /\.git/]
+      // followSymlinks: true
+    },
+    // recommended a and i as defaults
+    rsyncFlags: ['a', 'O', 'i']
+  }],
 }
 
 module.exports = config;
