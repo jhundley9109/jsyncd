c530775e90859967dd0300f9096dcdcd4b0255d1
Jacob Hundley <jhundley@eretailing.com>
2021-09-09 08:35:53 -0400

    A little organization

--- bin/index.js
+++ bin/index.js
@@ -11,14 +11,14 @@ import os from 'os';
 const processName = 'jsyncd';
 const defaultConfigFilePath = path.join(os.homedir(), '.config', processName, 'config.mjs');
 
-const optionParser = new JsyncdOptionParser({
-  processName: processName,
-  defaultConfigFilePath: defaultConfigFilePath,
-});
-
 parseOptionsAndRunProgram();
 
 async function parseOptionsAndRunProgram() {
+  const optionParser = new JsyncdOptionParser({
+    processName: processName,
+    defaultConfigFilePath: defaultConfigFilePath,
+  });
+
   let unparsed = await optionParser.parse().catch((err) => {
     console.log(`Error parsing cli options: ${err.message}`);
     process.exit();
@@ -26,59 +26,55 @@ async function parseOptionsAndRunProgram() {
 
   let configFilePath = unparsed.pop() || defaultConfigFilePath;
 
-  parseConfigFileAndStartProcess(configFilePath);
-}
-
-function parseConfigFileAndStartProcess(configFilePath) {
-  import(pathToFileURL(configFilePath)).then((configObj) => {
-    const config = configObj.default;
-
-    console.log(chalk.green(`Read configuration file: ${configFilePath}`));
-
-    config.logFile = optionParser.logFile.value() || config.logFile;
-
-    if (optionParser.ignoreInitial.value()) {
-      config.chokidarWatchOptions.ignoreInitial = true;
-    }
-
-    if (optionParser.debug.value()) {
-      config.debug = true;
-    }
-
-    if (optionParser.daemon.value() || config.daemonize) {
-      if (!config.logFile) {
-        console.log(chalk.red('-l, --log option required when using daemonize'));
-        process.exit();
-      }
-
-      console.log(chalk.yellow(`Going to detach process for ${processName}. Program output can be found at '${config.logFile}'`));
-
-      // pass cwd to work around an issue with the library passing the function process.cwd instead of the result
-      daemon({cwd: process.cwd()});
-    }
-
-    process.title = `${processName} ${configFilePath}`;
-
-    const jsyncd = new Jsyncd(config);
-
-    jsyncd.startSync().catch((err) => {
-      jsyncd.sendErrorToLog('Top level error, exiting the program. Exiting with error:');
-      jsyncd.sendToLog(err);
-      process.exit(1);
-    });
-
-    process.on('SIGINT', () => {
-      jsyncd.sendWarningToLog(jsyncd.getTimestamp() + ' Caught SIGINT. Terminating...');
-      process.exit();
-    });
-
-    process.on('SIGTERM', () => {
-      jsyncd.sendWarningToLog(jsyncd.getTimestamp() + ' Caught SIGTERM. Terminating...');
-      process.exit();
-    });
-  }).catch((err) => {
+  let configObj = await import(pathToFileURL(configFilePath)).catch((err) => {
     console.log(chalk.red(`Problem reading or parsing configuration file: ${configFilePath}.`));
     console.log(`${err}`);
     process.exit(1);
+  });
+
+  const config = configObj.default;
+
+  console.log(chalk.green(`Read configuration file: ${configFilePath}`));
+
+  config.logFile = optionParser.logFile.value() || config.logFile;
+
+  if (optionParser.ignoreInitial.value()) {
+    config.chokidarWatchOptions.ignoreInitial = true;
+  }
+
+  if (optionParser.debug.value()) {
+    config.debug = true;
+  }
+
+  if (optionParser.daemon.value() || config.daemonize) {
+    if (!config.logFile) {
+      console.log(chalk.red('-l, --log option required when using daemonize'));
+      process.exit();
+    }
+
+    console.log(chalk.yellow(`Process will detach. Output logged to '${config.logFile}'`));
+
+    // pass cwd to work around an issue with the library passing the function process.cwd instead of the result
+    daemon({cwd: process.cwd()});
+  }
+
+  process.title = `${processName} ${configFilePath}`;
+
+  const jsyncd = new Jsyncd(config);
+
+  jsyncd.startSync().catch((err) => {
+    jsyncd.sendErrorToLog('Top level error, exiting the program. Exiting with error:');
+    jsyncd.sendToLog(err);
+    process.exit(1);
+  });
+
+  process.on('SIGINT', () => {
+    jsyncd.sendWarningToLog(jsyncd.getTimestamp() + ' Caught SIGINT. Terminating...');
+    process.exit();
+  });
+
+  process.on('SIGTERM', () => {
+    jsyncd.sendWarningToLog(jsyncd.getTimestamp() + ' Caught SIGTERM. Terminating...');
+    process.exit();
   });
 }
